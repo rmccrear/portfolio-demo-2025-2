65174f33d390eab689c02c4f40bf3fb53f36236c
Robert McCreary Mannino <fooyinfighter@gmail.com>
2025-10-02 18:35:46 -0700

    add starter code for portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,61 @@
-
+// App.jsx
 import './App.css'
+import HeroImage from './components/HeroImage'
 
 function App() {
 
   return (
     <>
-      Hello world
+      <HeroImage />
+      {/* MAIN (Center website) */}
+      <div className="main">
+
+        <h1>MYLOGO.COM</h1>
+        <hr />
+
+        <h2>PORTFOLIO</h2>
+        <p>Resize the browser window to see the responsive effect.</p>
+
+        {/* Portfolio Gallery Grid */}
+        <div className="row">
+          <div className="column">
+            <div className="content">
+              <img src="/mountains.jpg" alt="Mountains" className="project-image" />
+              <h3>My Work</h3>
+              <p>Lorem ipsum..</p>
+            </div>
+          </div>
+          <div className="column">
+            <div className="content">
+              <img src="/lights.jpg" alt="Lights" className="project-image" />
+              <h3>My Work</h3>
+              <p>Lorem ipsum..</p>
+            </div>
+          </div>
+          <div className="column">
+            <div className="content">
+              <img src="/nature.jpg" alt="Nature" className="project-image" />
+              <h3>My Work</h3>
+              <p>Lorem ipsum..</p>
+            </div>
+          </div>
+          <div className="column">
+            <div className="content">
+              <img src="/mountains.jpg" alt="Mountains" className="project-image" />
+              <h3>My Work</h3>
+              <p>Lorem ipsum..</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="content">
+          <img src="/bear.jpg" alt="Bear" className="project-image" />
+          <h3>Some Other Work</h3>
+          <p>Lorem ipsum..</p>
+        </div>
+
+        {/* END MAIN */}
+      </div>
     </>
   )
 }
---
+++ src/components/HeroImage.jsx
@@ -0,0 +1,16 @@
+// ./components/HeroImage.jsx
+import "./HeroImage.css";
+
+export default function HeroImage() {
+    return (
+        <>
+            <div className="hero-image">
+                <div className="hero-text">
+                    <h1>I am Jane Doe</h1>
+                    <p>And I'm a Photographer</p>
+                    <button>Hire me</button>
+                </div>
+            </div>
+        </>
+    );
+}
